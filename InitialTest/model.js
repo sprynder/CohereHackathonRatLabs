cceebb39f826fb99504887ea02fe063dd3b36065
sprynder <liujames2003@gmail.com>
2022-12-18 14:50:12 -0600

    test model

---
+++ InitialTest/model.js
@@ -0,0 +1,38 @@
+const cohere = require('cohere-ai');
+cohere.init('Y9FxEPgNOaSUnIshWdfHmtVRMNaLyX5sRawtmR5o')
+
+
+let inputs = ["mad cuz bad", "i hate sid","saydie sc=ucks"];
+
+let examples = [
+    {text: 'you are hot trash', label: 'Toxic'},
+    {text: 'go to hell', label: 'Toxic'},
+    {text: 'get rekt moron', label: 'Toxic'},
+    {text: 'get a brain and use it', label: 'Toxic'},
+    {text: 'say what you mean, you jerk.', label: 'Toxic'},
+    {text: 'Are you really this stupid', label: 'Toxic'},
+    {text: 'I will honestly kill you', label: 'Toxic'},
+    {text: 'yo how are you', label: 'Benign'},
+    {text: 'I\'m curious, how did that happen', label: 'Benign'},
+    {text: 'Try that again', label: 'Benign'},
+    {text: 'Hello everyone, excited to be here', label: 'Benign'},
+    {text: 'I think I saw it first', label: 'Benign'},
+    {text: 'That is an interesting point', label: 'Benign'},
+    {text: 'I love this', label: 'Benign'},
+    {text: 'We should try that sometime', label: 'Benign'},
+    {text: 'You should go for it', label: 'Benign'}
+  ]
+
+async function model(){
+const response = await cohere.classify({
+  inputs: inputs,
+  examples: examples}
+);
+return response;
+}
+
+
+////////////////////////////////////////////////////////////////
+model().then((response)=>{
+    console.dir(response.body, {depth: null});
+});
