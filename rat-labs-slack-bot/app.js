9b05836517ec1694d65f4077beb00866373b2084
TAMUTim <chrometor@tamu.edu>
2022-12-20 22:31:00 -0600

    Add integration with ratAPI

--- rat-labs-slack-bot/app.js
+++ rat-labs-slack-bot/app.js
@@ -1,4 +1,5 @@
 const { App } = require("@slack/bolt");
+const axios = require("axios");
 require("dotenv").config();
 
 const app = new App({
@@ -12,6 +13,34 @@ app.command("/query", async ({ command, ack, say }) => {
 
         getUser(command.text).then(user_id => {
             findConversation(user_id).then(user_msg_arr => {
+                const sentiment_body = {
+                    inputs: user_msg_arr
+                }
+
+                axios.post('https://961d-2600-1700-31b1-4c00-18d3-35f9-696f-b015.ngrok.io/sentiment', 
+                    sentiment_body
+                )
+                .then(response => {
+                    console.log("FOR SENTIMENT:\n", response.data);
+                }, error => {
+                    console.log(error);
+                });
+                console.log(user_msg_arr);
+                
+                // search
+                const search_body = {
+                    inputs: user_msg_arr,
+                    query: "joy"
+                }
+
+                axios.post('https://961d-2600-1700-31b1-4c00-18d3-35f9-696f-b015.ngrok.io/search', 
+                    search_body
+                )
+                .then(response => {
+                    console.log("FOR SEARCHING:\n", response.data);
+                }, error => {
+                    console.log(error);
+                });
                 console.log(user_msg_arr);
             });
         });
